2e441882d77ede2d28895d571e24fc08ed43a3df
Tymon <s29787@pjwstk.edu.pl>
2026-05-07 11:41:41 +0200

    idk

--- zad1/main.py
+++ zad1/main.py
@@ -1,25 +1,49 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
+from io import StringIO
+
 
 def main() -> None:
+    with open("GDS2490.soft", encoding="utf-8") as file:
+        lines = file.readlines()
 
-    table = []
-    ap = False
-    try:
-        with open('GDS2490.soft') as file:
-            for line in file:
-                line = line.strip()
-                if not line or line.startswith("#") or line.startswith("^"):
-                    continue
-                if line.startswith("!dataset_table_begin"):
-                    ap = True
-                if line.startswith("!dataset_table_end"):
-                    ap = False
-                if ap:
-                    table.append(line)
-    except:
-        pass
+    start = next(i for i, line in enumerate(lines) if line.startswith("!dataset_table_begin")) + 1
+    end = next(i for i, line in enumerate(lines) if line.startswith("!dataset_table_end"))
+
+    table = "".join(
+        line for line in lines[start:end]
+        if not line.startswith(("#","^"))
+    )
+
+    df = pd.read_csv(StringIO(table), sep="\t")
+    non_smokers = [
+        "GSM114084",
+        "GSM114085",
+        "GSM114086",
+        "GSM114087",
+        "GSM114088",
+    ]
+
+    smokers = [
+        "GSM114078",
+        "GSM114079",
+        "GSM114080",
+        "GSM114081",
+        "GSM114082",
+        "GSM114083",
+    ]
+
+    df = df.melt([
+        "gene",
+        "sample",
+        "expression",
+        "group",
+
+    ]
+    )
+
+    print(df[smokers])
 
 
 if __name__ == "__main__":
